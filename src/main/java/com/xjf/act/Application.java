f08a25eb6b20daf054b03235087360fc6709cd66
xujiafei <2459610945@qq.com>
2020-01-07 11:08:14 +0800

    更新

--- src/main/java/com/xjf/act/Application.java
+++ src/main/java/com/xjf/act/Application.java
@@ -1,8 +1,16 @@
 package com.xjf.act;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 /**
  * 启动类
@@ -10,10 +18,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.xjf.act.mapper")
+@Slf4j
 public class Application {
 
+    @Bean
+    public HttpMessageConverters fastJsonConfigure(){
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        //日期格式化
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        converter.setFastJsonConfig(fastJsonConfig);
+        return new HttpMessageConverters(converter);
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        Environment env = context.getEnvironment();
+        log.info("====================================================================");
+//        log.info("Project Environment:     {}", env.getProperty("project.env"));
+        log.info("Server Port:             {}", env.getProperty("server.port"));
+//        log.info("Logging Level:           {}", env.getProperty("logback.level"));
+        log.info("Startup complete ...");
+        log.info("====================================================================");
     }
 
 
